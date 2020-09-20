8a18f57684a64e6a25f3c09e5bf1c0efe8c201c6
xanderyanov <xanderyanov@gmail.com>
2020-09-20 12:00:01 +0400

    12-00

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,4 +2,13 @@
 
 // });
 
-$(function () {});
+$(function () {
+  $(".go_to1").on("click", function () {
+    var scroll_el = $(".go_adr1");
+    headerHeight = $(".header__area").height();
+    if ($(scroll_el).length != 0) {
+      $("html, body").animate({ scrollTop: $(scroll_el).offset().top - headerHeight }, 1000);
+    }
+    return false;
+  });
+});
