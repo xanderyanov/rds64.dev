e925bf50519367d7acb22add8ccb001d7ba238ec
xanderyanov <xanderyanov@gmail.com>
2020-09-24 13:57:16 +0400

    13-57

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,4 +11,48 @@ $(function () {
     }
     return false;
   });
+
+  if ($(".carousel__container").length) {
+    var carousel = new Swiper(".carousel__container", {
+      slidesPerView: 1,
+      spaceBetween: 15,
+      slidesPerGroup: 1,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".carousel__btnNext",
+        prevEl: ".carousel__btnPrev",
+      },
+      breakpoints: {
+        1024: {
+          slidesPerView: 6,
+          spaceBetween: 15,
+        },
+        920: {
+          slidesPerView: 5,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+        600: {
+          slidesPerView: 3,
+          spaceBetween: 5,
+        },
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 5,
+        },
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 1,
+        },
+      },
+    });
+  }
 });
