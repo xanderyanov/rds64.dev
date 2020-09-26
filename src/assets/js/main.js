b9532a28c828dd4aa3da6a81f83788df68f90c97
xanderyanov <xanderyanov@gmail.com>
2020-09-26 12:33:56 +0400

    12-33

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -55,4 +55,62 @@ $(function () {
       },
     });
   }
+
+  if ($(".headerPaddingArea").length) {
+    var headerPaddingAreaHeight = $(".header__area").outerHeight();
+    $(".headerPaddingArea").css({ height: headerPaddingAreaHeight });
+  }
+
+  $(".content table").wrap('<div class="table_outer"></div>');
+
+  $("a[data-fancybox]").fancybox({
+    closeBtn: false,
+    arrows: true,
+    keyboard: true,
+    nextClick: true,
+    infobar: true,
+    protect: true,
+    backFocus: false, // убирает рассинхрон с swiper
+    nextEffect: "elastic",
+    prevEffect: "elastic",
+    padding: 0,
+    loop: true,
+    animationEffect: "zoom-in-out",
+    transitionEffect: "slide",
+    touch: {
+      vertical: true, // Allow to drag content vertically
+      momentum: true, // Continue movement after releasing mouse/touch when panning
+    },
+  });
+
+  $(".toTop").hide();
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 0) {
+      $(".toTop").fadeIn();
+    } else {
+      $(".toTop").fadeOut();
+    }
+  });
+  $(".toTop").on("click", function () {
+    $("body,html").animate({ scrollTop: 0 }, 400);
+    return false;
+  });
+
+  $(".menuButton").on("click", function (e) {
+    e.preventDefault();
+    if ($(this).hasClass("open")) {
+      $(this).removeClass("open");
+      $(".aMenu_js").slideUp(200);
+      $("body").removeClass("stop");
+    } else {
+      $(this).addClass("open");
+      $(".aMenu_js").slideDown(200);
+      $("body").addClass("stop");
+    }
+  });
+  $(".adaptiveMenu__close").on("click", function () {
+    $(".menuButton").removeClass("open");
+    $(".aMenu_js").slideUp(200);
+    $("body").removeClass("stop");
+  });
 });
